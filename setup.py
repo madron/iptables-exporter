5c0db3d46526a584542f3667a4ac92118e19c7bb
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2019-04-04 14:32:40 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'requirements.txt')) as f:
     requirements = f.read().splitlines()
 
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
