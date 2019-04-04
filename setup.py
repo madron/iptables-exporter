54ccd810fe8bcda04dad3bbd1efb7f7a9fb71365
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2019-04-04 14:36:51 +0200

    bump to 0.9.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="iptables-exporter",
-    version="0.3.2",
+    version="0.9.0",
     description='Prometheus iptables exporter',
     long_description=long_description,
 
